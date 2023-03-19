7929cbd6dfadcbe8c238ef5138d11dcae4f3f899
Yury Karshunou <yury.karshunou@upfitness.com>
2023-03-19 22:52:35 +0300

    return to old server.js

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3500
+const bodyParser = require('body-parser')
+const passport = require('passport')
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
-});
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(passport.initialize())
+require('./middleware/passport')(passport)
 
-const port = process.env.PORT || 3000;
+const routes = require('./settings/routes')
+routes(app)
 
-server.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-});
+app.listen(port, () => {
+    console.log(`App listen on port ${port}`);
+})
