1f11ee63707840571d090e1f8493641052bb1882
Yury Karshunou <yury.karshunou@upfitness.com>
2023-03-19 23:02:15 +0300

    return to old server.js 4

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
+const http = require('http')
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const passport = require('passport')
 
-// app.use(bodyParser.urlencoded({extended:true}))
-// app.use(bodyParser.json())
-// app.use(passport.initialize())
-// require('./middleware/passport')(passport)
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use(passport.initialize())
+require('./middleware/passport')(passport)
 
-// const routes = require('./settings/routes')
-// routes(app)
+const routes = require('./settings/routes')
+routes(app)
 
-app.listen(port, () => {
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!');
+});
+
+server.listen(port, () => {
     console.log(`App listen on port ${port}`);
 })
